f281137120d425b1a9a5cda9cfbabe58bc2bb939
SBrutskiy <brutzkiy@gmail.com>
2024-01-17 16:18:47 -0600

    First

---
+++ AquaPulse/src/Data.js
@@ -0,0 +1,26 @@
+export const UserData = [
+    {
+        testedDate:'11-20-2003',
+        ammoniaLevel: 0
+    },
+    {
+        testedDate:'11-21-2003',
+        ammoniaLevel: .25
+    },
+    {
+        testedDate:'11-23-2003',
+        ammoniaLevel: .5
+    },
+    {
+        testedDate:'11-26-2003',
+        ammoniaLevel: 1.5
+    } ,
+    {
+        testedDate:'11-28-2003',
+        ammoniaLevel: 3
+    },
+     {
+        testedDate:'11-30-2003',
+        ammoniaLevel: 2
+    }
+]
